e47ca2fbf9f167844bb6d61fa85c336816272c0d
Jonathan Edmunds <jonathan.edmunds@students.plymouth.ac.uk>
2019-03-23 15:30:51 +0000

    Update v2 app icon changed

--- Loober/Loober/Loober.Android/MainActivity.cs
+++ Loober/Loober/Loober.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Loober.Droid
 {
-    [Activity(Label = "Loober", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Loober", Icon = "@drawable/LooberIcon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
