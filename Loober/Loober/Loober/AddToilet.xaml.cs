17bbcda966a424c51cb82e46cbda303c60031077
Jonathan Edmunds <jonathan.edmunds@students.plymouth.ac.uk>
2019-03-24 13:35:47 +0000

    Update: Add new toilet now on seperate page android working well

---
+++ Loober/Loober/Loober/AddToilet.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Loober.Helper;
+using Loober.Models;
+using Plugin.Geolocator;
+using Xamarin.Forms.Maps;
+
+
+namespace Loober
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddToilet : ContentPage
+    {
+        FirebaseHelper firebaseHelper = new FirebaseHelper();
+        public AddToilet()
+        {
+            InitializeComponent();
+            toiletClean.Items.Add("Very Good");
+            toiletClean.Items.Add("Good");
+            toiletClean.Items.Add("Average");
+            toiletClean.Items.Add("Poor");
+            toiletClean.Items.Add("Very Poor");
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            await RetriveLocation();
+        }
+
+        private void CleanPicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var clean = toiletClean.Items[toiletClean.SelectedIndex];
+        }
+
+        private async Task RetriveLocation()
+        {
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 20;
+
+            var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(100000), null, true);
+
+            toiletLat.Text = position.Latitude.ToString();
+            toiletLong.Text = position.Longitude.ToString();
+
+        }
+
+        private async void BtnAddToilet_Clicked(object sender, EventArgs e)
+        {
+            await firebaseHelper.AddToilet(Convert.ToInt32(toiletId.Text), toiletName.Text, toiletDescription.Text, toiletLat.Text, toiletLong.Text);
+            toiletId.Text = string.Empty;
+            toiletName.Text = string.Empty;
+            toiletDescription.Text = string.Empty;
+            toiletLat.Text = string.Empty;
+            toiletLong.Text = string.Empty;
+
+            await DisplayAlert("Success", "Toilet Added Successfully", "OK");
+
+            await Navigation.PushAsync(new MainPage());
+            {
+
+            }
+        }
+    }
+}
--- Loober/Loober/Loober/MainPage.xaml.cs
+++ Loober/Loober/Loober/MainPage.xaml.cs
@@ -17,11 +17,7 @@ namespace Loober
         public MainPage()
         {
             InitializeComponent();
-            CleanPicker.Items.Add("Very Good");
-            CleanPicker.Items.Add("Good");
-            CleanPicker.Items.Add("Average");
-            CleanPicker.Items.Add("Poor");
-            CleanPicker.Items.Add("Very Poor");
+            
             btnGetLocation.Clicked += BtnGetLocation_Clicked;
         }
 
@@ -37,8 +33,7 @@ namespace Loober
 
             var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(100000), null, true);
 
-            txtLat.Text = position.Latitude.ToString();
-            txtLong.Text = position.Longitude.ToString();
+            
 
             MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(position.Latitude, position.Longitude)
                                 , Distance.FromMiles(1)));
@@ -52,65 +47,18 @@ namespace Loober
             await RetriveLocation();
         }
 
-        private void CleanPicker_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            var clean = CleanPicker.Items[CleanPicker.SelectedIndex];
-        }
+        
 
-        private async void BtnAdd_Clicked(object sender, EventArgs e)
-        {
-            await firebaseHelper.AddToilet(Convert.ToInt32(txtId.Text), txtName.Text, txtDescription.Text, txtLat.Text, txtLong.Text);
-            txtId.Text = string.Empty;
-            txtName.Text = string.Empty;
-            txtDescription.Text = string.Empty;
-            txtLat.Text = string.Empty;
-            txtLong.Text = string.Empty;
 
-            await DisplayAlert("Success", "Toilet Added Successfully", "OK");
-            var allToilets = await firebaseHelper.GetAllToilets();
-            lstToilets.ItemsSource = allToilets;
-        }
-
-        private async void BtnRead_Clicked(object sender, EventArgs e)
+        public async void BtnAddNewToilet_Clicked(object sender, EventArgs e)
         {
-            var toilet = await firebaseHelper.GetToilet(Convert.ToInt32(txtId.Text));
-            if (toilet != null)
+            await Navigation.PushAsync(new AddToilet());
             {
-                txtId.Text = toilet.ToiletId.ToString();
-                txtName.Text = toilet.Name;
-                txtDescription.Text = toilet.Description;
-                txtLong.Text = toilet.Longitude;
-                txtLat.Text = toilet.Latitude;
-                await DisplayAlert("Success", "Toilet Retrive Successfully", "OK");
-            }
-            else
-            {
-                await DisplayAlert("Success", "No Toilet Available", "OK");
+                
             }
         }
 
-        private async void BtnUpdate_Clicked(object sender, EventArgs e)
-        {
-            await firebaseHelper.UpdateToilet(Convert.ToInt32(txtId.Text), txtName.Text, txtDescription.Text, txtLat.Text, txtLong.Text);
-            txtId.Text = string.Empty;
-            txtName.Text = string.Empty;
-            txtDescription.Text = string.Empty;
-            txtLat.Text = string.Empty;
-            txtLong.Text = string.Empty;
-
-            await DisplayAlert("Success", "Toilet Updated Successfully", "OK");
-            var allToilets = await firebaseHelper.GetAllToilets();
-            lstToilets.ItemsSource = allToilets;
-        }
-
-        private async void BtnDelete_Clicked(object sender, EventArgs e)
-        {
-            await firebaseHelper.DeleteToilet(Convert.ToInt32(txtId.Text));
-            await DisplayAlert("Success", "Toilet Deleted Successfully", "OK");
-
-            var allToilets = await firebaseHelper.GetAllToilets();
-            lstToilets.ItemsSource = allToilets;
-        }
+        
 
     }
 }
